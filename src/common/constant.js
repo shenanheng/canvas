27b472ee327fda490b0c5f918338bc2492632340
shenah <251267741@qq.com>
2019-07-25 14:25:35 +0800

    增加

---
+++ src/common/constant.js
@@ -0,0 +1,3 @@
+// 整圆的弧度
+const WHOLE_CIRCULAR_RADIAN = 2 * Math.PI;
+const ONE_RADIAN = Math.PI / 180; // 1度等于多少弧度
--- src/countDown/countDown.js
+++ src/countDown/countDown.js
@@ -1,4 +1,51 @@
-class CountDown extends Canvas{
-    supper(id)
-    
+class CountDown extends Canvas {
+  constructor(id, data) {
+    super(id, data);
+    // 比例值的计算(为了放大与变小)是以1000为基准
+    this.scale = this.canvasWidth / 1000;
+    // 点阵图的集合
+    this.lattice = data;
+    // 点阵图一行有10个
+    this.latticeLength = 10;
+    // 倒计时小球球的半径
+    this.r = 6;
+    this.draw();
+  }
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    this.drawTime(); // 画倒计时的时钟
+    // this.ctx.restore();
+  }
+  start() {
+    this.draw();
+    setInterval(() => {
+      this.draw();
+    }, 1000);
+  }
+  // 画出倒计时的时间
+  drawTime() {
+    // this.ctx.save();
+    let hms = moment().format("h:mm:ss");
+    console.log(hms)
+    hms.split("").forEach((item, i) => {
+      this.drawNum(item, 2 * this.r * this.latticeLength * i);
+    });
+  }
+  // 画出相应的类型的点证图
+  drawNum(num, space) {
+    // 对应数字点阵值
+    let value = this.lattice[num];
+    this.ctx.fillStyle = "red";
+    value.forEach((item, i) => {
+      item.forEach((one, j) => {
+        let x = 2 * (this.r+1) * j + space;
+        let y = 2 * (this.r+1) * i;
+        if (one) {
+          this.ctx.beginPath();
+          this.ctx.arc(x, y, this.r, 0, WHOLE_CIRCULAR_RADIAN);
+          this.ctx.fill();
+        }
+      });
+    });
+  }
 }
